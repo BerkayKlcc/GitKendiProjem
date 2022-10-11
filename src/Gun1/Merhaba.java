6ed8393e7793d6f7d5e28251c640a5f2e2c65ff6
BerkayKlcc <mah_aybel@hotmail.com>
2022-10-11 11:38:52 +0300

    Ilk taskimi bitirdim. Taskin adi yazilir

---
+++ src/Gun1/Merhaba.java
@@ -0,0 +1,31 @@
+package Gun1;
+
+public class Merhaba {
+//      git init
+//      git add README.md
+//      git commit -m "first commit"
+//      git branch -M main
+//      git remote add origin https://github.com/..........
+//      git push -u origin main
+
+
+//      1- Git i başlat
+//      2- Takip edilecek dosyaları ekle
+//      3- Bölüm bittiğinde Etiketlemeyi yap
+//      4- uzak repository yi eşleştirelim
+//      5- değişiklikleri gönder
+
+
+//      1- VCS yi aktif et
+//      2- Takip edilecek dosyaları ekleme
+//      3- Commit e basıldı açılan ekranda
+//      yapılan işlemin adı yazıldı ve
+//      COMMIT e basıldı.
+//      4- Uzak Repo yu açalımö aynı zamanda
+//      eşleşme de olmuş oluyor.
+//      5- değişiklik, commit ve push
+
+public static void main(String[] args) {
+
+}
+}
